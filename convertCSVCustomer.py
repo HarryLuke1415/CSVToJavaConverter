5e1be1a52291ea73c613b24cfdb43d391f632288
HarryLuke1415 <harryluke1415@gmail.com>
2023-04-14 09:55:43 +1000

    Finished main functionality

--- convertCSVCustomer.py
+++ convertCSVCustomer.py
@@ -3,35 +3,46 @@ import csv
 with open('CSVFiles/customer.csv', newline='') as csvfile:
     reader = csv.DictReader(csvfile)
     with open('outputs/customerOutput.txt', 'w') as txtfile:
-        txtfile.write("List<Long> products = new ArrayList<>();\n")
+        txtfile.write("List<Long> bagItems = new ArrayList<>();\n")
+        txtfile.write("List<Long> likedItems = new ArrayList<>();\n")
+        txtfile.write("List<Long> dislikedItems = new ArrayList<>();\n")
+        txtfile.write("List<String> interestedCategories = new ArrayList<>();\n")
         x = 0
         for row in reader:
             x = x + 1
-            products = row["products"].split(",")
-            categories = row["categories"].split(",")
-            for product in products:
-                if product != "null":
-                    txtfile.write("products.add(" + product + "L);\n")
-            categories = row["categories"].split(",")
-            for category in categories:
-                if category != "null":
-                    txtfile.write("categories.add(\"" + category + "\");\n")
-            txtfile.write(f"Business business" + str(x) + " = new Business(")
+            bagItems = row["bagItems"].split(",")
+            likedItems = row["likedItems"].split(",")
+            dislikedItems = row["dislikedItems"].split(",")
+            interestedCategories = row["interestedCategories"].split(",")
+            for bagItem in bagItems:
+                if bagItem != "null":
+                    txtfile.write("bagItems.add(" + bagItem + "L);\n")
+            for likedItem in likedItems:
+                if likedItem != "null":
+                    txtfile.write("likedItems.add(" + likedItem + "L);\n")
+            for dislikedItem in dislikedItems:
+                if dislikedItem != "null":
+                    txtfile.write("dislikedItems.add(" + dislikedItem + "L);\n")
+            for interestedCategory in interestedCategories:
+                if interestedCategory != "null":
+                    txtfile.write("interestedCategories.add(\"" + interestedCategory + "\");\n")
+            txtfile.write(f"Customer customer" + str(x) + " = new Customer(")
             txtfile.write(f"\"{row['email']}\", ")
             txtfile.write(f"Country.{row['country']}, ")
             txtfile.write(f"UserRole.{row['userRole']}, ")
             txtfile.write(f"\"{row['password']}\", ")
-            txtfile.write(f"\"{row['businessName']}\", ")
-            txtfile.write(f"\"{row['ABN']}\", ")
-            txtfile.write(f"\"{row['phoneNumber']}\", ")
-            txtfile.write(f"null,")
-            txtfile.write(f"{row['rating']}, ")
-            txtfile.write("categories,")
-            txtfile.write("products,")
-            txtfile.write(f"\"{row['description']}\", ")
-            txtfile.write(f"\"{row['businessURL']}\"")
-            # Reviews go below
+            txtfile.write(f"\"{row['firstName']}\", ")
+            txtfile.write(f"\"{row['lastName']}\", ")
+            txtfile.write(f"\"{row['username']}\", ")
+            txtfile.write(f"Gender.{row['gender']}, ")
+            txtfile.write(f"{row['age']}, ")
+            txtfile.write("bagItems,")
+            txtfile.write("likedItems,")
+            txtfile.write("dislikedItems,")
+            txtfile.write("interestedCategories")
             txtfile.write(");\n")
-            txtfile.write("businessRepository.save(business" + str(x) + ");\n")
-            txtfile.write("products.clear();\n")
-            txtfile.write("categories.clear();\n")
+            txtfile.write("customerRepository.save(customer" + str(x) + ");\n")
+            txtfile.write("bagItems.clear();\n")
+            txtfile.write("likedItems.clear();\n")
+            txtfile.write("dislikedItems.clear();\n")
+            txtfile.write("interestedCategories.clear();\n")
